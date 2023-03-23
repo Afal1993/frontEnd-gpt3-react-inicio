d07f539f8e29d70124dd7903d2ce3cc4ae712caa
Alexandre Lima <afal1993profissional@gmail.com>
2023-03-23 13:13:53 -0300

    header finalizado

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,13 +14,14 @@ const Menu = () => (
 )
 
 const Navbar = () => {
+    
     const [ toggleMenu, setToggleMenu ] = useState(false);
 
     return (
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links">
                 <div className="gpt3__navbar-links_logo">
-                    <img src={logo} alt="logo" />
+                    <img src={logo} alt="logo"/>
                 </div>
                 <div className="gpt3__navbar-links_containers">
                     <Menu />
